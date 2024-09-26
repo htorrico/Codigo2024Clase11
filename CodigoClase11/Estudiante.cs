1aba0f5a5f2f489e5546ee3c29893c2e815ea754
Hugo Torrico <torricomarquezh@gmail.com>
2024-09-25 19:37:09 -0500

    Asociación por Lista.

---
+++ CodigoClase11/Estudiante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoClase11
+{
+    public class Estudiante
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Codigo { get; set; }
+
+        public List<Curso> Cursos { get; set; }
+
+        //public Curso Curso { get; set; }
+        //public Curso Curso2 { get; set; }
+        //public Curso Curso3 { get; set; }
+
+
+
+    }
+}
--- CodigoClase11/Program.cs
+++ CodigoClase11/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using CodigoClase11;
+
+Estudiante estudiante = new Estudiante
+{
+    Apellidos = "Sosa",
+    Nombres = "Edson",
+    Codigo = "20054124",
+    Cursos = new List<Curso>()
+};
+
+estudiante.Cursos.Add(new Curso
+{
+    Nombre = "Matemática",
+    Descripcion = "Descripción cualquiera",
+    Creditos = 5
+});
+estudiante.Cursos.Add(new Curso
+{
+    Nombre = "Historia",
+    Descripcion = "Descripción cualquiera",
+    Creditos = 4
+});
+
+estudiante.Cursos.Add(new Curso
+{
+    Nombre = "Religión",
+    Descripcion = "Descripción cualquiera",
+    Creditos = 3
+});
+
+estudiante.Cursos.Add(new Curso
+{
+    Nombre = "Biología",
+    Descripcion = "Descripción cualquiera",
+    Creditos = 5
+});
+
+
+foreach (var item in estudiante.Cursos)
+{
+    Console.WriteLine(item.Nombre);
+
+}
+
+
+
+
+//Estudiante estudiante = new Estudiante
+//{
+//    Apellidos = "Sosa",
+//    Nombres = "Edson",
+//    Codigo = "20054124",
+//    Curso = new Curso
+//    {
+//        Nombre = "Matemática",
+//        Descripcion = "Descripción cualquiera",
+//        Creditos = 5
+//    },
+//    Curso2= new Curso
+//    {
+//         Nombre="Historia",
+//          Descripcion="Descripcion",
+//           Creditos=4
+//    }
+
+//};
+
+//Console.WriteLine(estudiante.Curso.Nombre);
+//Console.WriteLine(estudiante.Curso2.Nombre);
+
+Console.Read();
